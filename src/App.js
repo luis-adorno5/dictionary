45453587d26a172c3c667ad24947ab5a0db51615
Luis Adorno <luis.adorno5@upr.edu>
2022-07-18 15:04:37 -0400

    Add.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12 text-center fw-bold fs-1 p-3 bg-primary text-white">
+            Pocket Dictionary
+          </div>
+          <div className="form-floating bg-primary py-3 pb-5 d-flex justify-content-center">
+            <input
+              type="text"
+              className="form-control-sm border-0 px-2 col-md-3 col-sm-4"
+              placeholder="Type your word"
+              id="floatingInput"
+            />
+            <button
+              className="btn btn-dark text-light col-md-1 col-sm-2 mx-2"
+            >
+              Search
+            </button>
+          </div>
+        <div className="fs-1 text-capitalize text-center fw-bold text-decoration-underline text-white bg-dark extra">
+          type a word in the box
+            </div>
+        </div>
+      </div>
+    </>
   );
-}
-
+};
+  
 export default App;
